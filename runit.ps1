e774c5ed4c849ef73356d607f58df9f613793bc0
Nick <mrrubiks0418@gmail.com>
2024-07-16 17:05:14 -0400

    Update runit.ps1

--- runit.ps1
+++ runit.ps1
@@ -1,7 +1,10 @@
 # Check if Python is installed
+# Ensure Python 3 is added to the PATH
+$env:Path += ";$env:LOCALAPPDATA\Programs\Python\Python312\Scripts"
+$env:Path += ";$env:LOCALAPPDATA\Programs\Python\Python312"
 $pythonInstalled = Get-Command python -ErrorAction SilentlyContinue
-
 if (-not $pythonInstalled) {
+
     Write-Output "Python is not installed. Downloading and installing Python..."
 
     # Set the URL to download Python installer
@@ -13,7 +16,6 @@ if (-not $pythonInstalled) {
 
     # Install Python silently
     Start-Process -FilePath $installerPath -ArgumentList "/quiet InstallAllUsers=0 PrependPath=1" -NoNewWindow -Wait
-
     # Check if Python was successfully installed
     $pythonInstalled = Get-Command python -ErrorAction SilentlyContinue
     if (-not $pythonInstalled) {
